8f9d1d128fb38dd8ec2272e94f4cbbff36bb65e8
quyetpv <quyetpv@bssd.vn>
2025-01-15 14:26:14 +0700

    base schedule

--- FE/src/router/student/routePaths.js
+++ FE/src/router/student/routePaths.js
@@ -1,4 +1,5 @@
-export default{
-    STUDENT_PROFILE:'/student/student-profile',
-    ACADEMIC_PERFORMANCE: '/student/academic-performance',
-}
+export default {
+  STUDENT_PROFILE: "/student/student-profile",
+  ACADEMIC_PERFORMANCE: "/student/academic-performance",
+  SCHEDULE_STUDENT: "/student/schedule",
+};
--- FE/src/router/student/routeStudent.js
+++ FE/src/router/student/routeStudent.js
@@ -2,14 +2,19 @@ import routeNames from "./routeNames";
 import routePaths from "./routePaths";
 
 export const routeStudent = [
-    {
-        path: routePaths.STUDENT_PROFILE,
-        name: routeNames.STUDENT_PROFILE,
-        component:()=>import('@/views/student/information/ProfileStudent.vue')
-    },
-    {
-        path:routePaths.ACADEMIC_PERFORMANCE,
-        name:routeNames.ACADEMIC_PERFORMANCE,
-        component:()=>import('@/views/student/academicPerformance/index.vue')
-    }
-]
+  {
+    path: routePaths.STUDENT_PROFILE,
+    name: routeNames.STUDENT_PROFILE,
+    component: () => import("@/views/student/information/ProfileStudent.vue"),
+  },
+  {
+    path: routePaths.ACADEMIC_PERFORMANCE,
+    name: routeNames.ACADEMIC_PERFORMANCE,
+    component: () => import("@/views/student/academicPerformance/index.vue"),
+  },
+  {
+    path: routePaths.SCHEDULE_STUDENT,
+    name: routeNames.SCHEDULE_STUDENT,
+    component: () => import("@/views/student/schedule/Schedule.vue"),
+  },
+];
