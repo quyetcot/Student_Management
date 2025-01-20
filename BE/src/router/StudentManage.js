3d1fe2addd8b2c33b703f3e56bb05e29a11babbb
dungnt <nguyentiendung11082004@gmail.com>
2025-01-20 16:41:57 +0700

    update studentmanage

--- BE/src/router/StudentManage.js
+++ BE/src/router/StudentManage.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { createStudent, deleteStudent, getAllStudent, getStudentById } from '../controllers/StudentsManage';
+import { createStudent, deleteStudent, getAllStudent, getStudentById, updateStudent } from '../controllers/StudentsManage';
 
 const StudentManageRouter = Router();
 StudentManageRouter.get('/', getAllStudent);
 StudentManageRouter.get('/:id', getStudentById);
 StudentManageRouter.post('/', createStudent);
-StudentManageRouter.get('/:id', deleteStudent);
+StudentManageRouter.delete('/:id', deleteStudent);
+StudentManageRouter.put('/:id', updateStudent);
 export default StudentManageRouter
